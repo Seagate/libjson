d43188e2d547a7d9a88075449cd0f84182d53aa7
Timothy Gilmer <Timothy.o.gilmer@seagate.com>
2022-04-28 18:32:20 -0600

    FEAT: build with resolution of 6 on floats

--- _internal/Source/NumberToString.h
+++ _internal/Source/NumberToString.h
@@ -109,8 +109,8 @@ public:
 
     #ifdef JSON_ISO_STRICT
 	   #define EXTRA_LONG
-	   #define FLOAT_STRING "%.2f"
-	   #define LFLOAT_STRING L"%.2f"
+	   #define FLOAT_STRING "%.6f"
+	   #define LFLOAT_STRING L"%.6f"
     #else
 	   #define EXTRA_LONG long
 	   #define FLOAT_STRING "%Lf"
