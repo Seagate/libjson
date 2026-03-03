eb173676c8af86ba3507765ab8dcc6b6cf2d4fd2
Swiss3003 <Gambet3003@gmail.com>
2026-03-03 15:37:31 -0700

    FEAT: adding equals_json() for strings and floats

--- _internal/TestSuite/UnitTest.h
+++ _internal/TestSuite/UnitTest.h
@@ -135,8 +135,27 @@ public:
 #define assertFloatEquals(one, two)\
 	assertTrue(UnitTest::_floatsAreEqual(one, two))
 
+inline bool equals_json(const json_string& s1, const json_string& s2)
+{
+    return s1 == s2;
+}
+
+inline bool equals_json(const json_string& s, json_number n)
+{
+    return std::stod(s) == n;
+}
+
+inline bool equals_json(json_number n, const json_string& s)
+{
+    return n == std::stod(s);
+}
+
+inline bool equals_json(json_number n1, json_number n2)
+{
+    return n1 == n2;
+}
 #define assertEquals(one, two)\
-    assertTrue((static_cast<json_number>(one)) == (static_cast<json_number>(two)))
+    assertTrue(equals_jso(one,two))
 
 #define assertNotEquals(one, two)\
     assertTrue((static_cast<json_number>(one)) != (static_cast<json_number>(two)))
