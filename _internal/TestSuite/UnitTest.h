954673a662865bff173970e37d8e0a750387a7d6
Swiss3003 <Gambet3003@gmail.com>
2026-03-06 09:29:21 -0700

    Trying std::string

--- _internal/TestSuite/UnitTest.h
+++ _internal/TestSuite/UnitTest.h
@@ -135,39 +135,7 @@ public:
 
 #define assertFloatEquals(one, two)\
 	assertTrue(UnitTest::_floatsAreEqual(one, two))
-inline bool equals_json(const char* s1, const char* s2)
-{
-    return strcmp(s1, s2) == 0;
-}
-inline bool equals_json(const std::string* s1, const char* s2)
-{
-    return s1 && s2 && *s1 == s2;
-}
 
-inline bool equals_json(const char* s1, const std::string* s2)
-{
-    return s1 && s2 && s1 == *s2;
-}
-
-inline bool equals_json(const std::string*& s1, const std::string*& s2)
-{
-    return s1 == s2;
-}
-
-inline bool equals_json(const json_string*& s, json_number n)
-{
-    return atof(s->c_str()) == n;
-}
-
-inline bool equals_json(json_number n, const json_string*& s)
-{
-    return n == atof(s->c_str());
-}
-
-inline bool equals_json(json_number n1, json_number n2)
-{
-    return n1 == n2;
-}
 #define assertEquals(one, two)\
     assertTrue(equals_json(one,two))
 
